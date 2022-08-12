fd1548bb0835b13a2ee2730a7187d3fb80671d91
Rob Koch <kochrt@gmail.com>
2022-08-12 14:58:10 -0700

    Add syntax highlighting basics

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,17 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { MarkwhenTimelineEditorProvider } from './timelineEditor';
+import "./semanticTokenProvider"
+import { legend, provider } from './semanticTokenProvider';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(MarkwhenTimelineEditorProvider.register(context))
+  vscode.languages.registerDocumentSemanticTokensProvider(
+    { language: "markwhen", scheme: "file" },
+    provider,
+    legend
+  );
+  
 }
 
 // this method is called when your extension is deactivated
---
+++ src/semanticTokenProvider.ts
@@ -0,0 +1,54 @@
+import * as vscode from "vscode";
+import { parse } from "@markwhen/parser";
+import { RangeType } from "@markwhen/parser/lib/Types";
+
+const tokenTypes = [
+  "comment",
+  "string",
+  "function",
+  "variable",
+  "parameter",
+  "property",
+  "keyword",
+  "type",
+];
+export const legend = new vscode.SemanticTokensLegend(tokenTypes, []);
+
+export const provider: vscode.DocumentSemanticTokensProvider = {
+  provideDocumentSemanticTokens(
+    document: vscode.TextDocument
+  ): vscode.ProviderResult<vscode.SemanticTokens> {
+    const tokensBuilder = new vscode.SemanticTokensBuilder(legend);
+    const markwhen = parse(document.getText());
+    markwhen.timelines.forEach((timeline) => {
+      timeline.ranges.forEach((range) => {
+        const vscodeRange = new vscode.Range(
+          range.lineFrom.line,
+          range.lineFrom.index,
+          range.lineTo.line,
+          range.lineTo.index
+        );
+        switch (range.type) {
+          case RangeType.Comment:
+            tokensBuilder.push(vscodeRange, "comment");
+            break;
+          case RangeType.DateRange:
+            tokensBuilder.push(vscodeRange, "type");
+            break;
+          case RangeType.Description:
+          case RangeType.Section:
+          case RangeType.Title:
+          case RangeType.View:
+            tokensBuilder.push(vscodeRange, "keyword");
+            break;
+          case RangeType.Tag:
+            tokensBuilder.push(vscodeRange, "property");
+          default:
+            tokensBuilder.push(vscodeRange, "string")
+        }
+      });
+    });
+
+    return tokensBuilder.build();
+  },
+};
