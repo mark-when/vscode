067b63d44d3e458c324c7a5089f6ac503cb7bfd2
Ryo Kaji <kaji@code.ac.jp>
2026-03-07 22:48:08 +0900

    annotate parsed object, fix its structure

--- src/semanticTokenProvider.ts
+++ src/semanticTokenProvider.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode";
 import { parse } from "./useParserWorker";
-import { RangeType } from "@markwhen/parser";
+import { ParseResult, RangeType } from "@markwhen/parser";
 
 const tokenTypes = [
   "comment",
@@ -18,39 +18,37 @@ export const legend = new vscode.SemanticTokensLegend(tokenTypes, []);
 
 export const provider: vscode.DocumentSemanticTokensProvider = {
   async provideDocumentSemanticTokens(
-    document: vscode.TextDocument
+    document: vscode.TextDocument,
   ): Promise<vscode.SemanticTokens> {
     const tokensBuilder = new vscode.SemanticTokensBuilder(legend);
 
-    const markwhen = await parse(document.getText());
-    markwhen.timelines.forEach((timeline: any) => {
-      timeline.ranges.forEach((range: any) => {
-        const from = document.positionAt(range.from);
-        const to = document.positionAt(range.to);
-        const vscodeRange = new vscode.Range(from, to);
-        switch (range.type) {
-          case RangeType.listItemIndicator:
-          case RangeType.CheckboxItemIndicator:
-            tokensBuilder.push(vscodeRange, "variable");
-            break;
-          case RangeType.Comment:
-            tokensBuilder.push(vscodeRange, "comment");
-            break;
-          case RangeType.DateRange:
-            tokensBuilder.push(vscodeRange, "type");
-            break;
-          case RangeType.Description:
-          case RangeType.Section:
-          case RangeType.Title:
-          case RangeType.View:
-            tokensBuilder.push(vscodeRange, "keyword");
-            break;
-          case RangeType.Tag:
-            tokensBuilder.push(vscodeRange, "property");
-          case RangeType.Recurrence:
-            tokensBuilder.push(vscodeRange, "class");
-        }
-      });
+    const markwhen = (await parse(document.getText())) as ParseResult;
+    markwhen.ranges.forEach((range: any) => {
+      const from = document.positionAt(range.from);
+      const to = document.positionAt(range.to);
+      const vscodeRange = new vscode.Range(from, to);
+      switch (range.type) {
+        case RangeType.listItemIndicator:
+        case RangeType.CheckboxItemIndicator:
+          tokensBuilder.push(vscodeRange, "variable");
+          break;
+        case RangeType.Comment:
+          tokensBuilder.push(vscodeRange, "comment");
+          break;
+        case RangeType.DateRange:
+          tokensBuilder.push(vscodeRange, "type");
+          break;
+        case RangeType.Description:
+        case RangeType.Section:
+        case RangeType.Title:
+        case RangeType.View:
+          tokensBuilder.push(vscodeRange, "keyword");
+          break;
+        case RangeType.Tag:
+          tokensBuilder.push(vscodeRange, "property");
+        case RangeType.Recurrence:
+          tokensBuilder.push(vscodeRange, "class");
+      }
     });
 
     return tokensBuilder.build();
